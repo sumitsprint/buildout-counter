b33e14e88870399a28549c9db576363082b3d8c6
Sumit Kumar. <ncr2099@gmail.com>
2025-10-05 07:01:17 +0530

    Counter App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"; 
 
-function App() {
-  const [count, setCount] = useState(0)
+function useCounter (iv = 0){
+  const [count, setCount] = useState(iv);
+  const Increment = () => (setCount(c => c + 1));
+  const Decrement = () => (setCount(c => c - 1));
+   
+  return {Increment, Decrement, count};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+
 }
 
-export default App
+
+
+export default function App(){
+  const {count, Increment, Decrement} = useCounter(0);
+
+  return(
+    <div className="App">
+      <h1>
+        Counter App
+      </h1>
+      <br/>
+      <p>Count: {count}</p>
+      <br/>
+      
+      <button onClick={Increment}>Increment</button>
+      
+      <button onClick={Decrement}>Decrement</button>
+    </div>
+  );
+
+}
